08e346a8eb0167c81736a3027db59e17e69898f1
moaaz97 <moaaz.briek97@gmail.com>
2023-03-29 00:45:29 +0200

    The PHP DOC

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -6,46 +6,79 @@ use App\Http\Requests\ProjectRequest;
 use App\Models\Project;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ProjectController extends Controller
 {
+    /**
+     * Retrieving all projects from the database and returns them to the view.
+     * The view then displays all projects in a table
+     * @return View
+     */
     public function index()
     {
         $projects = Project::all();
         return view('project.index', compact('projects'));
     }
 
+    /**
+     * @param Request $request
+     * @return mixed[]
+     * Retrieving tasks from the database based on the project ID passed in the request.
+     * If the project ID is ‘all’, it retrieves all tasks from the database and returns them as an array.
+     * Otherwise, it retrieves all tasks associated with the project ID and returns them as an array
+     */
     public function getProjectTasksUsingAjax(Request $request)
     {
-        $data = $request->all();
-        if ($data['project_id'] === 'all') {
+        if ($request->project_id === 'all') {
             $tasks = Task::with('project')
                 ->orderBy('priority', 'asc')
                 ->get()->toArray();
         } else {
             $tasks = Task::with('project')
                 ->orderBy('priority', 'asc')
-                ->where('project_id', $data['project_id'])
+                ->where('project_id', $request->project_id)
                 ->get()->toArray();
         }
         return $tasks;
     }
 
-    public function create()
+    /**
+     * Returns create view.
+     * * @return View
+     */
+    public function show()
     {
         return view('project.create');
     }
 
-    public function store(ProjectRequest $request)
+    /**
+     * @param ProjectRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     * Creates a new project.
+     * The function takes an instance of the ProjectRequest class as an argument.
+     * A new project is created with the name and description from the $request variable using the Project::create() method.
+     * Finally, the function redirects to the projects route.
+     */
+    public function create(ProjectRequest $request)
     {
-        $data = $request->all();
         Project::create([
-            'name' => $data['name'],
-            'description' => $data['description'],
+            'name' => $request->name,
+            'description' => $request->description,
         ]);
         return redirect()->route('projects');
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\RedirectResponse
+     * Edits an existing project.
+     * The function takes an instance of the Request class and an optional $id parameter as arguments.
+     * If the request method is POST, the function updates the project with the given ID with the new name and description from the request using the Project::update() method.
+     * If there is no project with the given ID, it returns an error view.
+     * If the request method is not POST, it retrieves the project with the given ID using the Project::where() method and returns a view to edit the project.
+     */
     public function edit(Request $request, $id = null)
     {
         if($request->method() === 'POST') {
@@ -67,6 +100,14 @@ class ProjectController extends Controller
         return view('errors');
     }
 
+    /**
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     * Deletes an existing project.
+     * The function is named delete and is defined as a public function.
+     * It takes an $id parameter as an argument. It retrieves the project with the given ID using the Project::where() method and deletes it using the delete() method.
+     * Finally, it redirects to the projects route.
+     */
     public function delete($id)
     {
         $project = Project::where('id', $id)->get()->first();
--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,10 +5,17 @@ namespace App\Http\Controllers;
 use App\Http\Requests\TaskRequest;
 use App\Models\Project;
 use App\Models\Task;
+use http\Env\Response;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
+    /**
+     * Retrieves all projects and tasks from the database and passes them to the view.
+     * The view then displays all tasks sorted by priority with a dropdown list of all available projects.
+     * @return View
+     */
     public function index()
     {
         $projects = Project::all();
@@ -16,13 +23,25 @@ class TaskController extends Controller
         return view('task.index', compact('tasks', 'projects'));
     }
 
-    public function create()
+    /**
+     * Retrieves all projects from the database and passes them to the view.
+     * The view then displays a form for creating a new task with a dropdown list of all available projects
+     * @return View
+     */
+    public function show()
     {
         $projects = Project::all();
         return view('task.create', compact('projects'));
     }
 
-    public function store(TaskRequest $request)
+    /**
+     *  Creates a new task. Retrieves all data from the form and creates a new task with the submitted data.
+     *  Validate the data based on specif rules in the TaskRequest.
+     *  The new task is then saved to the database and the user is redirected back to the task list page
+     * @param TaskRequest $request
+     * @return \Illuminate\Http\RedirectResponse|void
+     */
+    public function create(TaskRequest $request)
     {
         if ($request->post()) {
             $data = $request->all();
@@ -35,6 +54,14 @@ class TaskController extends Controller
         }
     }
 
+    /**
+     * @param $id
+     * @return View
+     * Updates an existing task.
+     * If the request is a POST request, it retrieves all data from the form and updates the task with the submitted data.
+     * The updated task is then saved to the database and the user is redirected back to the task list page.
+     * If the request is not a POST request, it retrieves the task with the given ID and displays it in an edit form along with all available projects.
+     */
     public function edit(Request $request, $id = null)
     {
         if ($request->post()) {
@@ -58,19 +85,31 @@ class TaskController extends Controller
 
     }
 
+    /**
+     * @param $id
+     * @return View|\Illuminate\Http\RedirectResponse
+     * Deletes an existing task with the given ID. If the task is deleted successfully, the user is redirected back to the task list page.
+     * If not, the user is redirected back to the error page.
+     */
     public function destroy($id)
     {
         $check = Task::where('id', $id)->delete();
         if ($check) {
             return redirect()->route('tasks');
         }
-        return redirect()->route('tasks')->withErrors($check);
+        return view('errors');
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     * Updates the priority of tasks. It loops through each task in the request and updates the priority of each task with the new index.
+     * The updated priority is then saved to the database. If all tasks are updated successfully, it returns a 204 No Content HTTP response
+     */
     public function reorder(Request $request)
     {
         foreach ($request->tasks as $index => $taskId) {
-            Task::where('id', $taskId)->update(['priority' => $index + 1,]);
+            Task::where('id', $taskId)->update(['priority' => $index + 1]);
         }
         return response()->noContent();
     }
